4f574cc1e6e3780aaa706e1816ae3c2c7a4a5857
kenderb <kenderbolivar@gmail.com>
2020-11-06 13:53:53 -0500

    Add file empty message

--- bin/main.rb
+++ bin/main.rb
@@ -23,15 +23,19 @@ module HelperMethods
 end
 if __FILE__ == $PROGRAM_NAME
   read_file = ReadFile.new(ARGV.first)
-  error_checker = ErrorChecker.new(read_file.read_scss_lines)
-  if error_checker.error_counter.positive?
-    puts "Founds #{error_checker.error_counter} errors".red
+  if read_file.read_scss_lines.instance_of?(String)
+    puts 'File empty'.red
   else
-    puts "Founds #{error_checker.error_counter} errors".green
+    error_checker = ErrorChecker.new(read_file.read_scss_lines)
+    if error_checker.error_counter.positive?
+      puts "Founds #{error_checker.error_counter} errors".red
+    else
+      puts "Founds #{error_checker.error_counter} errors".green
+    end
+    HelperMethods.display_error(error_checker.check_duplicate_includes, :_101)
+    HelperMethods.display_error(error_checker.check_bad_ending_atribute, :_201)
+    HelperMethods.display_error(error_checker.check_closing_brackets, :_301)
+    HelperMethods.display_error(error_checker.check_snake_on_selector_name, :_401)
+    puts ''
   end
-  HelperMethods.display_error(error_checker.check_duplicate_includes, :_101)
-  HelperMethods.display_error(error_checker.check_bad_ending_atribute, :_201)
-  HelperMethods.display_error(error_checker.check_closing_brackets, :_301)
-  HelperMethods.display_error(error_checker.check_snake_on_selector_name, :_401)
-  puts ''
 end
